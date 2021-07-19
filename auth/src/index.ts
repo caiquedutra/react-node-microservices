c56d7151c106a52d7d57d35bafa832be38010f4a
Caique Dutra Santos <caique@li-0ee284cc-2076-11b2-a85c-9ff141a0e119.ibm.com>
2021-07-19 16:57:42 -0300

    added ingress and error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,22 @@
 import express from 'express';
 
+import { signUpRouter } from './routes/signup';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { currentUserRouter } from './routes/current-user';
+import { errorHandler } from './middlewares/error-handle';
+
 const app = express();
 
 app.use(express.json());
 
+app.use(currentUserRouter);
+app.use(signUpRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(errorHandler);
 
 
-app.listen(3000, () =>{
-    console.log('Listening on port 4000');
-})
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
